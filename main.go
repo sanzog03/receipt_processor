3139745839d7dcb5252e0ab112e19b235cdefc2a
Sanjog Thapa <sanzog03@gmail.com>
2024-11-03 01:05:49 -0500

    refactored the database logic and structures into database package

--- main.go
+++ main.go
@@ -5,14 +5,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strings"
 	"time"
+
+	"receiptProcessor/database"
 
 	"github.com/gorilla/mux"
 )
 
+var receiptStore *database.ReceiptStore
+
 func main() {
 	r := mux.NewRouter()
+	receiptStore = database.NewReceiptStore()
 
 	r.HandleFunc("/receipt/process", ProcessReceipt).Methods("POST")
 	r.HandleFunc("/receipt/{id}/points", ReceiptPoints).Methods("GET")
@@ -25,8 +29,8 @@ func ReceiptPoints(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
 
-	receipt, ok := receipts[id]
-	if !ok {
+	receipt, error := receiptStore.Get(id)
+	if error != nil {
 		http.Error(w, "Receipt not found", http.StatusNotFound)
 		return
 	}
@@ -41,7 +45,7 @@ func ReceiptPoints(w http.ResponseWriter, r *http.Request) {
 func ProcessReceipt(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	fmt.Println(vars)
-	var receipt Receipt
+	var receipt database.Receipt
 	err := json.NewDecoder(r.Body).Decode(&receipt)
 	// receipts = append(receipts, receipt)
 	if err != nil {
@@ -51,54 +55,10 @@ func ProcessReceipt(w http.ResponseWriter, r *http.Request) {
 	}
 	// create a id
 	id := generateId()
-	receipts[id] = receipt
-	fmt.Println("new receipt with id:", id, " and value ", receipts)
+	receiptStore.Set(id, receipt)
+	fmt.Println("new receipt with id:", id, " and value ", receipt)
 }
 
 func generateId() string {
 	return fmt.Sprintf("%d", time.Now().UnixNano())
 }
-
-//  type definations
-
-type Item struct {
-	ShortDescription string  `json:"shortDescription"`
-	Price            float64 `json:"price,string"`
-}
-
-type Receipt struct {
-	Retailer     string     `json:"retailer"`
-	PurchaseDate CustomTime `json:"purchaseDate"`
-	PurchaseTime CustomTime `json:"purchaseTime"`
-	Items        []Item     `json:"items"`
-	Total        float64    `json:"total,string"`
-}
-
-// for special type date and time
-type CustomTime struct {
-	time.Time
-}
-
-func (ct *CustomTime) UnmarshalJSON(b []byte) error {
-	s := strings.Trim(string(b), "\"")
-	t, err := time.Parse("2006-01-02", s)
-	if err != nil {
-		t, err = time.Parse("15:04", s)
-		if err != nil {
-			return err
-		}
-	}
-	ct.Time = t
-	return nil
-}
-
-func (ct CustomTime) MarshalJSON() ([]byte, error) {
-	if ct.Time.Hour() == 0 && ct.Time.Minute() == 0 {
-		return []byte(fmt.Sprintf("\"%s\"", ct.Time.Format("2006-01-02"))), nil
-	}
-	return []byte(fmt.Sprintf("\"%s\"", ct.Time.Format("15:04"))), nil
-}
-
-// mock database. A hashmap for O(1) retrival
-
-var receipts = make(map[string]Receipt)
